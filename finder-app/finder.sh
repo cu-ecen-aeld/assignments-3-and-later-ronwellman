1890ddebf98ed61b5a9d9a822572a45513c80d19
Ron Wellman <ron.wellman01@gmail.com>
2025-06-19 22:03:51 -0400

    exercise 1 tested passing.

---
+++ finder-app/finder.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# ensure the requisite number of args
+if [ 2 -ne $# ]; then
+	echo "Usage: $0 <directory> <search string>"
+    exit 1
+fi
+
+filesdir=$1
+searchstr=$2
+
+files=$(find $filesdir -type f | wc -l)
+contains=$(grep -r $searchstr $filesdir | wc -l)
+
+echo "The number of files are $files and the number of matching lines are $contains" 
---
+++ finder-app/writer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# ensure the requisite number of args
+if [ 2 -ne $# ]; then
+	echo "Usage: $0 <filename> <writestr>"
+    exit 1
+fi
+
+writefile=$1
+writestr=$2
+basepath=$(dirname $writefile)
+
+# create parent directories if needed
+mkdir -p $basepath
+
+# create/overwrite file
+echo $2 > $1
